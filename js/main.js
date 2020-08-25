5f3ff7e543c066ad37526ab994d44863fc47840c
Cameron Ingham <camji555@gmail.com>
2020-08-25 13:58:23 -0700

    fix

--- js/main.js
+++ js/main.js
@@ -216,21 +216,6 @@ $('.portfolio-menu button').on('click', function(event) {
 	event.preventDefault();
 });
 
-
-
-
-// scrollToTop
-$.scrollUp({
-	scrollName: 'scrollUp', // Element ID
-	topDistance: '300', // Distance from top before showing element (px)
-	topSpeed: 300, // Speed back to top (ms)
-	animation: 'fade', // Fade, slide, none
-	animationInSpeed: 200, // Animation in speed (ms)
-	animationOutSpeed: 200, // Animation out speed (ms)
-	scrollText: '<i class="ti-rocket"></i>', // Text for element
-	activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-});
-
 // WOW active
 new WOW().init();
 
